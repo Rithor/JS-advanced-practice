d5f3b4aed5b47c56c2a0fc92728d3949999e2c2c
ViAnd <vitalii.andrianov@icloud.com>
2024-08-07 12:29:45 +0300

    modified:   src/components/search.js

--- src/components/search.js
+++ src/components/search.js
@@ -1,48 +1,46 @@
-import { Div } from '../common/div';
-import './search.css';
+import { Div } from "../common/div";
+import "./search.css";
 
 export class Search extends Div {
-
   constructor(state) {
     super();
     this.state = state;
   }
 
   #search() {
-    const value = this.el.querySelector('.search__input').value;
+    const value = this.el.querySelector(".search__input").value;
     if (value) {
       this.state.searchQuery = value;
     }
   }
 
-  #addListeneres() {
+  #addListeners() {
+    this.el.querySelector(".search__btn").addEventListener("click", () => {
+      this.#search();
+    });
     this.el
-      .querySelector('.search__btn')
-      .addEventListener('click', () => {
-        this.#search();
-      });
-    this.el
-      .querySelector('.search__input')
-      .addEventListener('keydown', (event) => {
-        if (event.code === 'Enter') {
+      .querySelector(".search__input")
+      .addEventListener("keydown", (event) => {
+        if (event.code === "Enter") {
           this.#search();
         }
       });
   }
 
   render() {
-    this.el.classList.add('search');
+    this.el.classList.add("search");
 
     this.el.innerHTML = `
 <div class="search__wrapper">
-    <input class="search__input" type="text" placeholder="Find a book or an author...." value="${this.state.searchQuery ? this.state.searchQuery : ''}">
+    <input class="search__input" type="text" placeholder="Find a book or an author...." value="${
+      this.state.searchQuery ? this.state.searchQuery : ""
+    }">
     <img src="./static/svg/search.svg" alt="Search icon">
 </div>
 <button class="search__btn"><img src="./static/svg/search-white.svg" alt="Search icon"></button>
     `;
 
-    this.#addListeneres();
+    this.#addListeners();
     return this.el;
   }
-
 }
